bbdc22c0943a74d797e826b467a4bbf131ca2ebf
Moussa Clarke <moussaclarke@gmail.com>
2022-06-26 11:58:08 +0100

    chore: suppress irrelevant bump error

--- src/Shell/bump.sh
+++ src/Shell/bump.sh
@@ -22,13 +22,13 @@ esac
 
 #Get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+HAS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #Create new tag
 NEW_VERSION="$MAJOR.$MINOR.$PATCH"
 
 # Only tag if no tag already
-if [ -z "$NEEDS_TAG" ]; then
+if [ -z "$HAS_TAG" ]; then
     # if style.css exists, this is a theme
     if [ -f style.css ]; then
         echo "Updating theme version to $NEW_VERSION"
